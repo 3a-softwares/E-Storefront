d5973e4909365649ea82cac59a17b389f5ccea2b
Abhishek Jain <abhishek.jain1@hp.com>
2026-01-11 10:35:58 +0530

    coupon service

--- services/coupon-service/api/index.js
+++
@@ -1,5 +0,0 @@
-// Vercel Serverless Function Handler
-// Uses the pre-bundled code from .vercel-build/index.js
-const handler = require('../.vercel-build/index.js');
-
-module.exports = handler.default || handler;
